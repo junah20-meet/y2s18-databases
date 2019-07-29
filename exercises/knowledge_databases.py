6d9699156a201b2f050465f2da0adde36dfd7edb
myname21-meet <myname21@meet.mit.edu>
2019-07-29 11:32:00 -0400

    cmommittedd

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,42 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(topic, title, rating):
+	topic_object = Knowledge(
+		topic = topic,
+		title = title,
+		rating = rating)
+	session.add(topic_object)
+	session.commit()
+	
 
 def query_all_articles():
-	pass
+	knowledge = session.query(Knowledge).all()
+	return(knowledge)
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(new_topic):
+	knowledge = session.query(Knowledge).filter_by(topic=new_topic).all()
+	return(knowledge)
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(new_topic):
+	session.query(Knowledge).filter_by(topic=new_topic).delete()
+	session.commit()
+
+delete_article_by_topic("food")
 
 def delete_all_articles():
-	pass
+	knowledge = session.query(Knowledge).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(new_rating):
+	Knowledge_object = session.query(Knowledge).filter_by(rating = new_rating).first()
+	Knowledge_object.rating = new_rating
+	session.commit()
+	
+#add_article("animal", 3, 7)
+#add_article("juna", 6, 7)
+print(query_article_by_topic("animal"))
+#delete_article_by_topic("juna")
+delete_all_articles()
+print(query_all_articles())
+
